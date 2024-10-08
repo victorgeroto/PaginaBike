c08c6f41aacaa163634231d4252d2055b9552ad2
victorgeroto <132359789+victorgeroto@users.noreply.github.com>
2024-10-08 11:06:59 -0300

    Add files via upload

--- LojaBike/js/script.js
+++ LojaBike/js/script.js
@@ -1,8 +1,26 @@
 function ModalSucess(){
     Swal.fire({
-        title: "Ops!",
-        text: "Sua Internet não está funcionando",
-        icon: "question"
+        title: "Produto Adicionado ao Carrinho!",
+        text: "Vá para o carrinho ver seu produto.",
       });
 
+  }
+  function ModalWarning(){
+    Swal.fire({
+        title: "Deseja Finalizar a compra?",
+        text: "Você tem certeza disso?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Finalizar!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Compra Finalizada!",
+            text: "Sua compra foi finalizada com sucesso.",
+            icon: "success"
+          });
+        }
+      });
   }
